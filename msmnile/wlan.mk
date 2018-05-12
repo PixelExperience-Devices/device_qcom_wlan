0d05dff3fef4e7e2ba3695225977b57ed61dfbd0
Purushottam Kushwaha <pkushwah@codeaurora.org>
2018-05-12 13:35:08 +0530

    ICM: copy icm.conf to device.
    This adds a new configuration file icm.conf which is copied to
    device in /vendor/etc/wifi/ location.
    
    Change-Id: Ie222ced1d299d3fb315a5c36f8a084b41cdfbfd5
    CRs-Fixed: 2087598

--- msmnile/wlan.mk
+++ msmnile/wlan.mk
@@ -5,4 +5,5 @@ PRODUCT_COPY_FILES += \
     device/qcom/wlan/msmnile/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
 				device/qcom/wlan/msmnile/wifi_concurrency_cfg.txt:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wifi_concurrency_cfg.txt \
 				device/qcom/wlan/msmnile/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
-				device/qcom/wlan/msmnile/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf
+				device/qcom/wlan/msmnile/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
+				device/qcom/wlan/msmnile/icm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/icm.conf
