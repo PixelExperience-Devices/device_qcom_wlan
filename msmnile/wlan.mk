ca0e8ed14c509e44693ab8fa7f7933796bb02d88
Anurag Das <anurdas@codeaurora.org>
2018-07-19 15:52:22 +0530

    wifi: Push aware permission files to device
    Push aware permission files to the device
    for enabling wifi aware feature.
    
    Change-Id: Ib90f60d9a3b8530dcc53e58d66fadebdbfa23566
    CRs-Fixed: 2281656

--- msmnile/wlan.mk
+++ msmnile/wlan.mk
@@ -8,7 +8,8 @@ PRODUCT_COPY_FILES += \
     device/qcom/wlan/msmnile/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
 				device/qcom/wlan/msmnile/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
 				device/qcom/wlan/msmnile/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
-				device/qcom/wlan/msmnile/icm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/icm.conf
+				device/qcom/wlan/msmnile/icm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/icm.conf \
+                                frameworks/native/data/etc/android.hardware.wifi.aware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.aware.xml
 
 # Enable STA + SAP Concurrency.
 WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
