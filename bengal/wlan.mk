a619d5fc014e0f6b3f999053bc7410e8bdea9080
Purushottam Kushwaha <pkushwah@codeaurora.org>
2020-09-03 11:41:11 +0530

    Bengal: Enable Passpoint for bengal target.
    This adds permission attribute to all Passpoint feature for bengal
    target.
    
    Change-Id: I77ee84e6692aadec1fcf10e424f72d87d3066ff8
    CRs-Fixed: 2758882

--- bengal/wlan.mk
+++ bengal/wlan.mk
@@ -16,7 +16,8 @@ PRODUCT_COPY_FILES += \
 	device/qcom/wlan/bengal/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
 	device/qcom/wlan/bengal/icm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/icm.conf \
 	frameworks/native/data/etc/android.hardware.wifi.aware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.aware.xml \
-	frameworks/native/data/etc/android.hardware.wifi.rtt.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.rtt.xml
+	frameworks/native/data/etc/android.hardware.wifi.rtt.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.rtt.xml \
+	frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml
 
 # WLAN specific aosp flag
 TARGET_USES_AOSP_FOR_WLAN := false
