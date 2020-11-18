b2cff938ca8fabb5298c650b5e6a9436b497a5a5
Mohammad Asaad Akram <asadkrm@codeaurora.org>
2020-11-18 14:50:08 +0530

    lahaina: add vendor_cmd.xml file for wifi
    Change-Id: I9469c2b45f08ee84cd66f36f62689ad74e5b3be3
    CRs-Fixed: 2819001

--- lahaina/wlan.mk
+++ lahaina/wlan.mk
@@ -17,6 +17,7 @@ PRODUCT_COPY_FILES += \
 				device/qcom/wlan/lahaina/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
 				device/qcom/wlan/lahaina/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
 				device/qcom/wlan/lahaina/icm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/icm.conf \
+				device/qcom/wlan/lahaina/vendor_cmd.xml:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/vendor_cmd.xml \
                                 frameworks/native/data/etc/android.hardware.wifi.aware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.aware.xml \
                                 frameworks/native/data/etc/android.hardware.wifi.rtt.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.rtt.xml \
                                 frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml
