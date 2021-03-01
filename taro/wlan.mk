11a79dddf3a0572e877d59296a0a74c9b3161983
Mahesh Kumar Kalikot Veetil <mkalikot@codeaurora.org>
2021-03-01 15:26:56 -0800

    taro: Add multichip support for wlan
    Handle multiple chipsets as listed using 'TARGET_WLAN_CHIP'.
    For multichip configuration, respective modules and config files
    will be copied.
    
    WLAN_KBUILD_OPTIONS_<chip> variable injects KBUILD options per
    chip.
    
    Change-Id: I67a30c10515d8657dcb58f29075999a9d2f36a27
    CRs-Fixed: 2890539

--- taro/wlan.mk
+++ taro/wlan.mk
@@ -1,9 +1,33 @@
+#
+# TARGET_WLAN_CHIP directs QCACLD driver to be built for that particular
+# chipset(s). It can take multiple supported chipsets. Please refer QCACLD
+# driver code for supported chipsets.
+#
+# Default behaviour is invoked if TARGET_WLAN_CHIP is not defined.
+#
+# It also installs chip specific INI files.
+#
+# e.g. TARGET_WLAN_CHIP := qca6490 qca6390
+#	builds qca_cld3_qca6490.ko and qca_cld3_qca6390.ko
+#
+#	Copies configuration files from device/qcom/wlan/taro/ to
+#	$(TARGET_COPY_OUT_VENDOR)/etc/wifi/ like,
+#
+#	WCNSS_qcom_cfg_qca6490.ini -> qca6490/WCNSS_qcom_cfg.ing
+#	WCNSS_qcom_cfg_qca6390.ini -> qca6390/WCNSS_qcom_cfg.ing
+#
+#
 WLAN_CHIPSET := qca_cld3
 
 #WPA
 WPA := wpa_cli
 
-PRODUCT_PACKAGES += $(WLAN_CHIPSET)_wlan.ko
+# Package chip specific ko files if TARGET_WLAN_CHIP is defined.
+ifneq ($(TARGET_WLAN_CHIP),)
+	PRODUCT_PACKAGES += $(foreach chip, $(TARGET_WLAN_CHIP), $(WLAN_CHIPSET)_$(chip).ko)
+else
+	PRODUCT_PACKAGES += $(WLAN_CHIPSET)_wlan.ko
+endif
 PRODUCT_PACKAGES += wifilearner
 PRODUCT_PACKAGES += $(WPA)
 PRODUCT_PACKAGES += lowirpcd
@@ -12,8 +36,18 @@ PRODUCT_PACKAGES += qsh_wifi_test
 #Enable WIFI AWARE FEATURE
 WIFI_HIDL_FEATURE_AWARE := true
 
+# Copy chip specific INI files if TARGET_WLAN_CHIP is defined
+ifneq ($(TARGET_WLAN_CHIP),)
+	PRODUCT_COPY_FILES += \
+			      $(foreach chip, $(TARGET_WLAN_CHIP), \
+			      device/qcom/wlan/taro/WCNSS_qcom_cfg_$(chip).ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/$(chip)/WCNSS_qcom_cfg.ini)
+else
+	PRODUCT_COPY_FILES += \
+			      device/qcom/wlan/taro/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+
+endif
+
 PRODUCT_COPY_FILES += \
-    device/qcom/wlan/taro/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
 				device/qcom/wlan/taro/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
 				device/qcom/wlan/taro/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
 				device/qcom/wlan/taro/icm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/icm.conf \
@@ -37,3 +71,15 @@ QC_WIFI_HIDL_FEATURE_DUAL_STA := true
 
 #Disable DMS MAC address feature in cnss-daemon
 TARGET_USES_NO_DMS_QMI_CLIENT := true
+
+
+ifneq ($(TARGET_WLAN_CHIP),)
+
+	# Inject Kbuild options per chip
+	#
+	# Select proper chip configuration for building WLAN driver
+	# module. Currently driver supports only one chip
+	# configuration per build.
+	#
+	# e.g WLAN_KBUILD_OPTIONS_qca6490 := CONFIG_CNSS_QCA6490=y
+endif
