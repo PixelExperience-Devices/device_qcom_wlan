973f533c2b3b1abd9d3703f99a3323a2ccf0ea22
Rongjing Liao <liaor@codeaurora.org>
2021-09-22 14:44:32 +0800

    sm6150_au: Add support for SAP+SAP+STA concurrency
    Customer wants below WLAN interface combination:
    2.4G SAP + 5G SAP(LOHS) + STA, P2P is no need.
    To support this case, add one new interface combination.
    
    Change-Id: Id81e271cacff4bdf3eba25fc33b2005000fbe697

--- sm6150_au/wlan.mk
+++ sm6150_au/wlan.mk
@@ -44,7 +44,8 @@ endif
 # AOSP: interface combinations
 WIFI_HAL_INTERFACE_COMBINATIONS := {{{STA}, 1}, {{AP}, 1}, {{P2P}, 1}},\
                                    {{{STA}, 1}, {{NAN}, 1}}, \
-                                   {{{STA}, 2}, {{AP}, 1}}
+                                   {{{STA}, 2}, {{AP}, 1}}, \
+                                   {{{STA}, 1}, {{AP}, 2}}
 
 # Override WLAN configurations
 # # Usage:
