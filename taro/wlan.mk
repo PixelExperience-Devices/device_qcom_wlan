55917ce558734734bf1b1209bb9342c175435d9a
Amarnath Hullur Subramanyam <amarnath@codeaurora.org>
2021-10-07 13:51:54 -0700

    Wlan: Add wlan control app dut tool
    Add wlan control app dut tool to wlan.mk
    
    Change-Id: Ib899dcea882081fd88357193b350371044fb7a9e
    CRs-Fixed: 3046878

--- taro/wlan.mk
+++ taro/wlan.mk
@@ -39,6 +39,7 @@ PRODUCT_PACKAGES += lowirpcd
 PRODUCT_PACKAGES += qsh_wifi_test
 PRODUCT_PACKAGES += init.vendor.wlan.rc
 PRODUCT_PACKAGES += wificfrtool
+PRODUCT_PACKAGES += ctrlapp_dut
 
 #Enable WIFI AWARE FEATURE
 WIFI_HIDL_FEATURE_AWARE := true
