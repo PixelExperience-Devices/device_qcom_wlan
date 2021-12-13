abea7efad5e5c8515b44a6cbc82fd50fbb003d15
Sai Pavan Akhil Remella <quic_saipavan@quicinc.com>
2021-12-13 16:21:36 +0530

    wlan: sdm660: Disable wifi-aware service
    This commit disables the wifi-service by not including
    xml file i.e. android.hardware.wifi.aware.xml and by
    removing the compile time flag
    i.e. WIFI_HIDL_FEATURE_AWARE.
    
    Change-Id: I37bed8a240f94a7a5391c602728691c1295d7073
    CRs-Fixed: 3072904
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- sdm660_64/wlan.mk
+++ sdm660_64/wlan.mk
@@ -10,14 +10,10 @@ PRODUCT_PACKAGES += \
         wifilearner
 PRODUCT_PACKAGES += $(WPA)
 
-#Enable WIFI AWARE FEATURE
-WIFI_HIDL_FEATURE_AWARE := true
-
 PRODUCT_COPY_FILES += \
 	device/qcom/wlan/sdm660_64/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
 	device/qcom/wlan/sdm660_64/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
 	device/qcom/wlan/sdm660_64/icm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/icm.conf \
-	frameworks/native/data/etc/android.hardware.wifi.aware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.aware.xml \
 	frameworks/native/data/etc/android.hardware.wifi.rtt.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.rtt.xml \
 	frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml
 
@@ -37,7 +33,3 @@ WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
 
 # Enable SAP + SAP Feature.
 QC_WIFI_HIDL_FEATURE_DUAL_AP := true
-
-# Enable vendor properties.
-PRODUCT_PROPERTY_OVERRIDES += \
-	wifi.aware.interface=wifi-aware0
