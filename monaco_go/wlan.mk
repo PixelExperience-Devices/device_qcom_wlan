f81f299906da142ff913ff22bffea12759b1fb1b
Sai Pavan Akhil Remella <quic_saipavan@quicinc.com>
2022-09-26 13:08:16 +0530

    wlan: Add support for wear targets
    This change includes monaco, monaco_go, monaco_go_aon
    targets support in wlan-service-mcc.lnx.1.0.
    
    Change-Id: Ibe78b6167ee7bb6a867288afd77afcb17e4e114a
    CRs-Fixed: 3299757

---
+++ monaco_go/wlan.mk
@@ -0,0 +1,52 @@
+WLAN_CHIPSET := qca_cld3
+
+# WLAN wear specific defconfig
+WLAN_PROFILE := wear
+
+#WPA
+WPA := wpa_cli
+
+PRODUCT_PACKAGES += $(WLAN_CHIPSET)_wlan.ko
+PRODUCT_PACKAGES += wifilearner
+PRODUCT_PACKAGES += $(WPA)
+PRODUCT_PACKAGES += ctrlapp_dut
+
+ifneq ($(TARGET_SUPPORTS_WEARABLES),true)
+#Enable WIFI AWARE FEATURE
+WIFI_HIDL_FEATURE_AWARE := true
+endif
+
+ifeq ($(BOARD_WLAN_DIR),)
+    BOARD_WLAN_DIR := device/qcom/wlan
+endif
+
+PRODUCT_COPY_FILES += \
+	$(BOARD_WLAN_DIR)/monaco_go/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
+	$(BOARD_WLAN_DIR)/monaco_go/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
+	$(BOARD_WLAN_DIR)/monaco_go/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
+	$(BOARD_WLAN_DIR)/monaco_go/icm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/icm.conf
+
+ifneq ($(TARGET_SUPPORTS_WEARABLES),true)
+PRODUCT_COPY_FILES += \
+	frameworks/native/data/etc/android.hardware.wifi.aware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.aware.xml \
+	frameworks/native/data/etc/android.hardware.wifi.rtt.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.rtt.xml
+endif
+
+# WLAN specific aosp flag
+TARGET_USES_AOSP_FOR_WLAN := false
+
+# WLAN specific memory flag
+WLAN_TARGET_MONACO_HAS_LOW_RAM := true
+
+# Enable STA + SAP Concurrency.
+WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
+
+# Enable SAP + SAP Feature.
+QC_WIFI_HIDL_FEATURE_DUAL_AP := true
+
+#Enable cal delete feature
+TARGET_CAL_DATA_CLEAR := true
+
+# Enable vendor properties.
+PRODUCT_PROPERTY_OVERRIDES += \
+	wifi.aware.interface=wifi-aware0
